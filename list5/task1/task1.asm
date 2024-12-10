db446a0331ef65818382e1e33a023d25117dc334
ShrikeBin <grahamka.ha@gmail.com>
2024-12-10 16:46:43 +0000

    suffering cd

--- list5/task1/task1.asm
+++ list5/task1/task1.asm
@@ -1,91 +1,96 @@
 section .data
-    buffer db 20         ; Bufor na wejście
+    buffer db 0          ; Buffer for input
     buffer_len equ 20
     prompt db "Enter a number: ", 0
     result_msg db "Sum of digits: ", 0
     newline db 10, 0
 
 section .bss
-    sum resd 1           ; Przechowywanie sumy cyfr
+    sum resd 1           ; To store the sum of digits
 
 section .text
     global _start
 
 _start:
-    ; Wyświetl prompt
+    ; Display prompt
     mov eax, 4
     mov ebx, 1
     mov ecx, prompt
     mov edx, 17
     int 0x80
 
-    ; Wczytaj wejście
+    ; Read input
     mov eax, 3
     mov ebx, 0
     mov ecx, buffer
     mov edx, buffer_len
     int 0x80
 
-    ; Inicjalizacja sumy
-    xor esi, esi          ; Indeks w buforze
-    xor eax, eax          ; Aktualna suma
+    ; Null-terminate input to avoid overrun issues
+    mov byte [buffer + eax - 1], 0x00
+
+    ; Initialize sum and index
+    xor esi, esi          ; Index in buffer
+    xor eax, eax          ; Current sum
 
 sum_digits:
-    movzx ecx, byte [buffer + esi]
-    cmp ecx, 10           ; Sprawdź znak końca wprowadzenia
-    je print_result
-    sub ecx, '0'          ; Konwertuj ASCII na cyfrę
-    add eax, ecx          ; Dodaj cyfrę do sumy
-    inc esi               ; Kolejny znak
+    movzx ecx, byte [buffer + esi] ; Load character
+    cmp ecx, 0x00         ; Check for null terminator
+    je print_result       ; If null, stop
+    sub ecx, '0'          ; Convert ASCII to digit
+    add eax, ecx          ; Add digit to sum
+    inc esi               ; Move to next character
     jmp sum_digits
 
 print_result:
-    mov [sum], eax        ; Zapisz sumę cyfr
+    mov [sum], eax        ; Store sum in memory
 
-    ; Wyświetl wynik
+    ; Display result message
     mov eax, 4
     mov ebx, 1
     mov ecx, result_msg
-    mov edx, 16
+    mov edx, 15
     int 0x80
 
-    ; Zamień liczbę na ASCII
+    ; Convert number to ASCII and print
     mov eax, [sum]
     call print_number
 
-    ; Wyświetl nową linię
+    ; Print newline
     mov eax, 4
     mov ebx, 1
     mov ecx, newline
     mov edx, 1
     int 0x80
 
-    ; Wyjście z programu
+    ; Exit program
     mov eax, 1
     xor ebx, ebx
     int 0x80
 
 print_number:
-    ; Konwertuj liczbę na ASCII i wyświetl
-    push eax
-    xor ecx, ecx
+    ; Convert number to ASCII and display
+    xor ecx, ecx          ; Digit counter
 print_digit_loop:
-    xor edx, edx
-    mov ebx, 10
-    div ebx
-    add dl, '0'
-    push dx
-    inc ecx
-    test eax, eax
+    xor edx, edx          ; Clear remainder
+    mov ebx, 10           ; Divisor
+    div ebx               ; Divide eax by 10
+    add dl, '0'           ; Convert remainder to ASCII
+    push dx               ; Store on stack
+    inc ecx               ; Increment digit counter
+    test eax, eax         ; Check if quotient is 0
     jnz print_digit_loop
 
 print_digits:
-    pop dx
-    mov [buffer], dl
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, buffer
-    mov edx, 1
-    int 0x80
-    loop print_digits
+    push ecx             ; Save loop counter `ecx` on the stack
+    pop dx                ; Retrieve digit from stack
+    mov [buffer], dl      ; Store digit in buffer
+    mov eax, 4            ; sys_write
+    mov ebx, 1            ; stdout
+    mov ecx, buffer       ; Address of the digit buffer
+    mov edx, 1            ; Length = 1 character
+    int 0x80              ; Call kernel to print digit
+    pop ecx              ; Restore loop counter `ecx`
+    dec ecx               ; Decrement loop counter
+    jnz print_digits       ; Continue loop until counter reaches 0
     ret
