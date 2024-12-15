c828caf2ede435f0623cf2743fd540a1f1298aa4
ShrikeBin <grahamka.ha@gmail.com>
2024-12-15 10:36:07 +0100

    fuck task4

--- list5/task4/task4.asm
+++ list5/task4/task4.asm
@@ -1,108 +1,73 @@
 %include        '../functions.asm'
+
 section .data
-    sieve_size equ 100001             ; Rozmiar tablicy (100000 + 1, bo 0 jest ignorowane)
-    prime_msg db "Prime: ", 0
-    newline db 10, 0
+    newline db 10, 0           ; Znak nowej linii (ASCII 0x0A)
+    limit dd 100000            ; Górny limit do którego znajdujemy liczby pierwsze
 
 section .bss
-    sieve resb sieve_size             ; Tablica do oznaczania liczb (0 = potencjalnie pierwsza, 1 = nie pierwsza)
+    primes resb 100000        ; Bufor na flagi liczb pierwszych (do 100000 / 8 = ~125000 bajtów)
 
 section .text
     global _start
 
 _start:
-    ; Inicjalizacja sita - ustaw wszystkie wartości na 0 (potencjalnie pierwsze)
-    mov ecx, sieve_size
-    xor eax, eax                      ; Wartość do zapisania (0)
-    mov edi, sieve
-    rep stosb
+    ; Inicjalizacja - wypełnienie tablicy liczb flagami (wszystkie liczby oznaczamy jako potencjalne pierwsze)
+    mov ecx, 0
+    mov eax, 100000
+    mov edi, primes
 
-    ; Wykonaj sito Eratostenesa
-    mov ecx, 2                        ; Pierwsza liczba pierwsza
+init_loop:
+    mov byte [edi + ecx], 1   ; Ustawiamy wszystkie liczby jako potencjalne pierwsze
+    inc ecx
+    cmp ecx, eax
+    jl init_loop
+
+    ; Sito Eratostenesa - eliminujemy liczby złożone
+    mov ecx, 2                 ; Pierwsza liczba do sprawdzenia
+
 sieve_loop:
-    cmp ecx, sieve_size               ; Czy przekroczyliśmy zakres?
-    jge print_primes
+    mov eax, ecx                ; Aktualna liczba `ecx`
+    mov edx, ecx                ; Kopia liczby do mnożenia
 
-    ; Sprawdź, czy aktualna liczba jest oznaczona jako pierwsza
-    mov al, byte [sieve + ecx]
-    cmp al, 1                         ; Jeśli 1, to liczba jest oznaczona jako nie pierwsza
-    je next_number
+    ; Oblicz wartość `n^2` i sprawdź, czy mieści się w granicach
+    imul edx, ecx
+    cmp edx, 100000             ; Jeśli n^2 > 100000, kończ algorytm
+    jg finish
 
-    ; Zaznacz wielokrotności tej liczby jako nie pierwsze
-    mov ebx, ecx                      ; Zapisujemy aktualną liczbę w ebx
-    mov edx, ecx                      ; Zaczynamy od kwadratu liczby
-    imul edx, edx
-mark_multiples:
-    cmp edx, sieve_size               ; Czy wielokrotność jest poza zakresem?
-    jge next_number
-    mov byte [sieve + edx], 1         ; Oznacz wielokrotność jako nie pierwszą
-    add edx, ebx                      ; Przejdź do następnej wielokrotności
-    jmp mark_multiples
+    mov ebx, edx
+    mov esi, primes
 
-next_number:
-    inc ecx                           ; Następna liczba
+eliminate_loop:
+    mov al, byte [esi + ebx]    ; Sprawdzamy wartość flagi w tablicy
+    cmp al, 0
+    je continue_outer
+
+    mov byte [esi + ebx], 0     ; Wyzerowujemy flagę dla liczb złożonych
+
+continue_outer:
+    add ebx, ecx
+    cmp ebx, limit
+    jl eliminate_loop
+
+    inc ecx
     jmp sieve_loop
 
-print_primes:
-    mov ecx, 2                        ; Zacznij od 2
+finish:
+    ; Teraz wypisujemy liczby pierwsze na konsolę
+    mov ecx, 2
+    mov edi, primes
+
 print_loop:
-    cmp ecx, sieve_size               ; Czy przekroczyliśmy zakres?
-    jge exit
+    mov al, byte [edi + ecx]     ; Sprawdzamy flagę dla liczby
+    cmp al, 0
+    je not_prime
 
-    ; Sprawdź, czy liczba jest pierwsza
-    mov al, byte [sieve + ecx]
-    cmp al, 0                         ; Jeśli 0, liczba jest pierwsza
-    jne next_print
+    mov eax, ecx                  ; Wartość liczb pierwszych
+    call iprintLF                 ; Wypisujemy liczbę na konsoli
 
-    ; Wyświetl "Prime: "
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, prime_msg
-    mov edx, 7
-    int 0x80
+not_prime:
+    add ecx, 1                    ; Przesuwamy po kolejnej liczbie w tablicy
+    cmp ecx, 100000
+    jl print_loop                 ; Powtarzamy proces, dopóki nie osiągniemy limitu liczb
 
-    ; Wyświetl liczbę
-    mov eax, ecx                      ; Przechowujemy aktualną liczbę w eax
-    call iprintLF
-
-    ; Wyświetl nową linię
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, newline
-    mov edx, 1
-    int 0x80
-
-next_print:
-    inc ecx                           ; Przejdź do następnej liczby
-    jmp print_loop
-
-exit:
-    ; Wyjście z programu
-    mov eax, 1
-    xor ebx, ebx
-    int 0x80
-
-print_decimal:
-    ; Funkcja pomocnicza do wyświetlania liczby dziesiętnej
-    push eax
-    xor ecx, ecx
-print_digit_loop:
-    xor edx, edx
-    mov ebx, 10
-    div ebx                           ; eax = eax / 10, edx = eax % 10
-    add dl, '0'                       ; Zamień cyfrę na ASCII
-    push dx
-    inc ecx
-    test eax, eax
-    jnz print_digit_loop
-
-print_digits:
-    pop dx
-    mov [sieve], dl
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, sieve
-    mov edx, 1
-    int 0x80
-    loop print_digits
-    ret
+    call quit
